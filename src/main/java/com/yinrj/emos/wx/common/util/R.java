eaaa86e046f1d1189fe266fac72be25084e073b5
YinRJ <yin_rj@qq.com>
2021-02-28 21:50:45 +0800

    完成请求的统一请求封装类

---
+++ src/main/java/com/yinrj/emos/wx/common/util/R.java
@@ -0,0 +1,58 @@
+package com.yinrj.emos.wx.common.util;
+
+import org.apache.http.HttpStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author yinrongjie
+ * @version 1.0
+ * @description 统一的返回
+ * @date 2021/2/28
+ */
+public class R extends HashMap<String, Object> {
+    public R() {
+        put("code", HttpStatus.SC_OK);
+        put("msg", "success");
+    }
+
+    /**
+     * 可以链式调用
+     */
+    public R put(String key, Object value) {
+        super.put(key, value);
+        return this;
+    }
+
+    public static R ok() {
+        return new R();
+    }
+
+    public static R ok(String msg) {
+        R r = new R();
+        r.put("msg", msg);
+        return r;
+    }
+
+    public static R ok(Map<String, Object> map) {
+        R r = new R();
+        r.putAll(map);
+        return r;
+    }
+
+    public static R error(int code, String msg) {
+        R r = new R();
+        r.put("code", code);
+        r.put("msg", msg);
+        return r;
+    }
+
+    public static R error(String msg) {
+        return error(HttpStatus.SC_INTERNAL_SERVER_ERROR, msg);
+    }
+
+    public static R error() {
+        return error(HttpStatus.SC_INTERNAL_SERVER_ERROR, "未知异常，请联系管理员。");
+    }
+}
