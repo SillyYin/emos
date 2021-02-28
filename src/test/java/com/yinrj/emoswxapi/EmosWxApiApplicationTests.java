71d833753d737c92ba07d856a863e875145c3268
YinRJ <yin_rj@qq.com>
2021-02-28 19:35:11 +0800

    增加数据库的配置信息

--- src/test/java/com/yinrj/emoswxapi/EmosWxApiApplicationTests.java
+++ src/test/java/com/yinrj/emoswxapi/EmosWxApiApplicationTests.java
@@ -1,6 +1,8 @@
 package com.yinrj.emoswxapi;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
